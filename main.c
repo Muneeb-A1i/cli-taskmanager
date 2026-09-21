dea2c8573fa37530f7f9d77bdcc836a6ca96c00a
Muneeb <mmuneebali67@gmail.com>
2026-09-21 20:05:41 +0100

    Added basic menu

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+	printf("----- Task Manager -----\n");
+	printf("1. Add Task\n");
+	printf("2. Remove Task\n");
+	printf("3. List Tasks\n");
+	printf("4. Mark Task Complete\n");
+	printf("5. Search Tasks\n");
+	printf("6. Save\n");
+	printf("7. Load\n");
+	return 0;
+}
