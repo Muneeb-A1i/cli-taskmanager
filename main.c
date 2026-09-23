36d26488c4ae6d3cb7c3fbe65b0d3849a2332f42
Muneeb <mmuneebali67@gmail.com>
2026-09-23 19:13:26 +0100

    Added switches

--- main.c
+++ main.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 int main(void){
 
 	int choice;
@@ -19,5 +17,34 @@ int main(void){
 	scanf("%d", &choice);
 
 	printf("%d\n", choice);
+
+	switch(choice){
+		case 1:
+			printf("Task added!\n");
+			break;
+		case 2:
+			printf("Task removed\n");
+			break;
+		case 3:
+			printf("List of Tasks\n");
+			break;
+		case 4:
+			printf("Task marked complete\n");
+			break;
+		case 5:
+			printf("Searched for a task\n");
+			break;
+		case 6:
+			printf("Saved!\n");
+			break;
+		case 7:
+			printf("Loaded successfully!\n");
+			break;
+		default:
+			printf("ERROR! ENTER A NUMBER BETWEEN 1-7 ONLY!!!\n");
+
+
+
+}
 	return 0;
 }
